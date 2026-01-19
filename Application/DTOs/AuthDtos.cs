68852abea7c497af7936fcf6b23808139fcee3b0
preciouswoko <73164614+preciouswoko@users.noreply.github.com>
2026-01-19 10:08:20 +0100

    fix error

--- Application/DTOs/AuthDtos.cs
+++ Application/DTOs/AuthDtos.cs
@@ -31,7 +31,6 @@ namespace Application.DTOs
     //    public string Email { get; set; } = string.Empty;
     //    public string Password { get; set; } = string.Empty;
     //}
-    using System.ComponentModel.DataAnnotations;
 
 public class RegisterDto
     {
